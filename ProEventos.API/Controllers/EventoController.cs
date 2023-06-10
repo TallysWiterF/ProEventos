211416eb3b010a9565516c9ae5952cf42c537558
Tallys Witer <tallyswiter@hotmail.com>
2023-06-10 19:26:10 -0300

    Atualizando API
    Criando métodos de Get e GetById

--- ProEventos.API/Controllers/EventoController.cs
+++ ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -6,10 +7,40 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> _evento = new Evento[]
         {
-            return "value";
+            new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 6",
+                Local = "Belo Horizonte",
+                Lote = "Primeiro Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Tema = "Angular e Suas novidades",
+                Local = "São Paulo",
+                Lote = "Segundo Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto2.png"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{eventoId}")]
+        public IEnumerable<Evento> GetById(int eventoId)
+        {
+            return _evento.Where(x => x.EventoId == eventoId);
         }
     }
 }
---
+++ ProEventos.API/Models/Evento.cs
@@ -0,0 +1,13 @@
+﻿namespace ProEventos.API.Models;
+
+public class Evento
+{
+    public int EventoId { get; set; }
+    public string Local { get; set; } = string.Empty;
+    public string DataEvento { get; set; } = string.Empty;
+    public string Tema { get; set; } = string.Empty;
+    public int QtdPessoas { get; set; }
+    public string Lote { get; set; } = string.Empty;
+    public string ImagemURL { get; set; } = string.Empty;
+}
+
