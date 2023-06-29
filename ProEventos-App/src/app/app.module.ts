162b43b976a73cfcf70b05124c671d3b5f62046b
Tallys Witer <tallyswiter@hotmail.com>
2023-06-28 23:54:40 -0300

    Estilizando tela principal e criando filtros.

--- ProEventos-App/src/app/app.module.ts
+++ ProEventos-App/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 
 @NgModule({
   declarations: [
@@ -23,6 +27,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos-App/src/app/eventos/eventos.component.ts
@@ -4,13 +4,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
 
+
   public eventos: any = [];
-  widthImg: number = 50;
-  marginImg: number = 2;
+  public eventosFiltrados: any = [];
+  larguraImagem: number = 100;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtroLista: string): any {
+    filtroLista = filtroLista.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: {tema: string, local: string}) => evento.tema.toLocaleLowerCase().indexOf(filtroLista) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtroLista) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,13 +40,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos() {
 
     this.http.get('https://localhost:7072/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventosFiltrados = this.eventos = response
+      },
       error => console.log(error)
     );
-
   }
-
 }
