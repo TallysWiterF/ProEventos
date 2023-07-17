fd655a540f0334ddc7c7ece1a77a3e1c81afe7c0
Tallys Witer <tallyswiter@hotmail.com>
2023-07-17 06:41:23 -0300

    Criação de novos componentes e rotas
    Implementando rotas no projeto e criação de novos componentes.

--- ProEventos-App/src/app/app.module.ts
+++ ProEventos-App/src/app/app.module.ts
@@ -9,22 +9,33 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
       PalestrantesComponent,
+      ContatosComponent,
+      DashboardComponent,
+      PerfilComponent,
+      TituloComponent,
       NavComponent,
       DateTimeFormatPipe,
    ],
--- ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { EventoService } from '../services/evento.service';
-import { Evento } from '../models/Evento';
+import { EventoService } from '../../services/evento.service';
+import { Evento } from '../../models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -50,7 +50,7 @@ export class EventosComponent implements OnInit {
     this.spinner.show();
     this.getEventos();
     setTimeout(() => {
-    }, 1000);
+    }, 300);
   }
 
   public alterarImagem(): void {
